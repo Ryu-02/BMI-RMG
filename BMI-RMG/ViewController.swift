80d51b0c0218e11ad6eb22cb76d9a42ce702ed4c
Ryu-02 <ymq02110192@gmail.com>
2024-11-07 16:21:24 +0900

    Initial Commit

---
+++ BMI-RMG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI-RMG
+//
+//  Created by 유명규 on 11/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BMI-RMGTests/BMI_RMGTests.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_RMGTests.swift
+//  BMI-RMGTests
+//
+//  Created by 유명규 on 11/7/24.
+//
+
+import Testing
+@testable import BMI_RMG
+
+struct BMI_RMGTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
